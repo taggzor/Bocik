465cb42969f20384a5d3922d320adfb8b383415d
taggzor <multimaxtroll@gmail.com>
2019-08-08 20:00:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = ".";
+
 
 client.on('ready', () => {
 console.log('I am ready!');
@@ -8,8 +10,17 @@ console.log('I am ready!');
 
 client.on('message', msg => {
 
-if (msg.content === 'ping') {
-  msg.reply('pong');
+
+
+if (!msg.content.startsWith(prefix)) return;
+
+var args = msg.content.substring(prefix.length).split(" ");
+
+switch (args[0]){
+  case "ping":
+      msg.reply('pong');
+      break;
+  
 }
 
 
