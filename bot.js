c12666f8fcb13f63de95a4cec55aa3f4d8551e85
taggzor <multimaxtroll@gmail.com>
2019-08-10 19:25:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,15 +132,17 @@ switch (args[0].toLowerCase()){
           queue: []
         };
         var server = servers[msg.guild.id];
-        server.queue.push("https://www.youtube.com/watch?v=7-qGKqveZaM");
+  
         szukaj(search,msg);
-        
+        setTimeout(function(){
+          if(!msg.guild.voiceConnection) msg.member.voiceChannel.join().then(function(connection){
+            play(connection, msg);
+          });
+      }, 3000);
         
       
-      if(!msg.guild.voiceConnection) msg.member.voiceChannel.join().then(function(connection){
-        play(connection, msg);
-      });
-        msg.channel.send(".skip");
+      
+        
         break;
       }
       if(!servers[msg.guild.id]) servers[msg.guild.id] = {
